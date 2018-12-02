b9d7a08c4e83c69eee357eb8953b87854a5fc85c
dns <dserbin96@yandex.ru>
2018-12-02 23:20:04 +0300

    add title

--- app/src/main/java/com/example/danilserbin/motionlayoutexample/example/erc/ERCMenuActivity.kt
+++ app/src/main/java/com/example/danilserbin/motionlayoutexample/example/erc/ERCMenuActivity.kt
@@ -1,7 +1,7 @@
 package com.example.danilserbin.motionlayoutexample.example.erc
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.example.danilserbin.motionlayoutexample.R
 
 class ERCMenuActivity : AppCompatActivity() {
@@ -9,5 +9,7 @@ class ERCMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ercmenu)
+
+        supportActionBar?.title = getString(R.string.erc)
     }
 }
--- app/src/main/java/com/example/danilserbin/motionlayoutexample/example/erc/ERCSignInActivity.kt
+++ app/src/main/java/com/example/danilserbin/motionlayoutexample/example/erc/ERCSignInActivity.kt
@@ -1,7 +1,7 @@
 package com.example.danilserbin.motionlayoutexample.example.erc
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.example.danilserbin.motionlayoutexample.R
 
 class ERCSignInActivity : AppCompatActivity() {
@@ -9,6 +9,8 @@ class ERCSignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_erc_sign_in)
+
+        supportActionBar?.title = getString(R.string.erc)
 
     }
 
